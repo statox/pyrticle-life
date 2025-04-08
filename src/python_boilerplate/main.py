f82206068f2d8aec0d94bd623b1204c220d68b39
statox <me@statox.fr>
2025-04-08 20:29:30 +0200

    1 000 000 particles - 60 FPS

--- src/python_boilerplate/main.py
+++ src/python_boilerplate/main.py
@@ -41,18 +41,11 @@ prog = ctx.program(
     """,
 )
 
-nb_particles = 20_000
+nb_particles = 1_000_000
 
 # Buffer and Vertex Array
 vbo = ctx.buffer(reserve=nb_particles * 2 * 4)  # float32 = 4 bytes
 vao = ctx.simple_vertex_array(prog, vbo, "in_position")
-
-
-# Function to handle wrapping
-def wrap_around(p: tuple[float, float]) -> tuple[float, float]:
-    x = (p[0] + 1) % 2 - 1
-    y = (p[1] + 1) % 2 - 1
-    return (x, y)
 
 
 def draw(particles: NDArray[np.float32]) -> None:
@@ -80,7 +73,8 @@ def main() -> None:
             last_time = current_time
 
         particles += velocities
-        particles = np.array([wrap_around(p) for p in particles], dtype=np.float32)
+        # Wrap around
+        particles = (particles + 1) % 2 - 1
 
         draw(particles)
         glfw.set_window_title(window, f"{len(particles)} Particles - FPS: {fps}")
