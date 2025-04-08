2f4c88e46d60c1be388dd4c8450a0b2b269aceba
statox <me@statox.fr>
2025-04-08 19:26:24 +0200

    1000 particles - 22 FPS

--- src/python_boilerplate/main.py
+++ src/python_boilerplate/main.py
@@ -1,8 +1,8 @@
 # source .venv/bin/activate
 # uv run python src/python_boilerplate/main.py
+# uv run python -m cProfile -s tottime src/python_boilerplate/main.py
 
 import time
-from typing import Tuple
 
 import glfw
 import numpy as np
@@ -10,10 +10,39 @@ import OpenGL.GL as gl
 
 
 # Function to handle wrapping
-def wrap_around(p: Tuple[float, float]) -> Tuple[float, float]:
+def wrap_around(p: tuple[float, float]) -> tuple[float, float]:
     x = (p[0] + 1) % 2 - 1  # Wrap around horizontally
     y = (p[1] + 1) % 2 - 1  # Wrap around vertically
     return (x, y)
+
+
+def draw(particles: np.ndarray, fps: int, window: glfw._GLFWwindow) -> None:
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+
+    # Draw white borders
+    gl.glColor3f(1, 1, 1)
+    gl.glLineWidth(2)
+    gl.glBegin(gl.GL_LINE_LOOP)
+    gl.glVertex2f(-1, -1)
+    gl.glVertex2f(1, -1)
+    gl.glVertex2f(1, 1)
+    gl.glVertex2f(-1, 1)
+    gl.glEnd()
+
+    radius = 0.01
+    for p in particles:
+        gl.glBegin(gl.GL_TRIANGLE_FAN)
+        gl.glColor3f(1, 0, 0)
+        gl.glVertex2f(p[0], p[1])  # Center
+        for angle in np.linspace(0, 2 * np.pi, 20):
+            gl.glVertex2f(p[0] + radius * np.cos(angle), p[1] + radius * np.sin(angle))
+        gl.glEnd()
+
+    # Display FPS
+    glfw.set_window_title(window, f"{len(particles)} Particles - FPS: {fps}")
+
+    glfw.swap_buffers(window)
+    glfw.poll_events()
 
 
 def main() -> None:
@@ -52,39 +81,11 @@ def main() -> None:
             frame_count = 0
             last_time = current_time
 
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-
-        # Draw white borders
-        gl.glColor3f(1, 1, 1)
-        gl.glLineWidth(2)
-        gl.glBegin(gl.GL_LINE_LOOP)
-        gl.glVertex2f(-1, -1)
-        gl.glVertex2f(1, -1)
-        gl.glVertex2f(1, 1)
-        gl.glVertex2f(-1, 1)
-        gl.glEnd()
-
         # Update particle positions and apply wrapping
         particles += velocities
         particles = np.array([wrap_around(p) for p in particles])
 
-        radius = 0.01
-        for p in particles:
-            gl.glBegin(gl.GL_TRIANGLE_FAN)
-            gl.glColor3f(1, 0, 0)
-            gl.glVertex2f(p[0], p[1])  # Center
-            for angle in np.linspace(0, 2 * np.pi, 20):
-                gl.glVertex2f(
-                    p[0] + radius * np.cos(angle), p[1] + radius * np.sin(angle)
-                )
-            gl.glEnd()
-
-        # Display FPS
-        glfw.set_window_title(window, f"{len(particles)} Particles - FPS: {fps}")
-
-        glfw.swap_buffers(window)
-        glfw.poll_events()
-        # time.sleep(1 / 60)  # 60 FPS
+        draw(particles, fps, window)
 
     glfw.terminate()
 
